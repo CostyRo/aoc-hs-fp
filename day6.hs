ec7f773e053df42f50f50ea9dbf8d7ba0726b502
unknown <ioncotinosu12@gmail.com>
2021-12-19 19:00:50 +0200

    sixth problem

---
+++ day6.hs
@@ -0,0 +1,13 @@
+nextDay :: Int -> Int 
+nextDay 0=6
+nextDay n=n-1
+
+simulate :: Int -> [Int] -> [Int]
+simulate 0 list=list
+simulate n list=simulate (n-1) ([nextDay x | x <- list]++[8 | x <- [0..length(filter (== 0) list)-1]])
+
+main :: IO()
+main=do
+  let array=[3,4,3,1,2]
+  let days=80
+  print (length (simulate days array))
